fb5a25c2934763786822443f66aab2855d1397f6
Rory Anderson <rory@jragon.co.uk>
2013-07-14 14:40:07 +0100

    logs in when creates a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
-class UsersController < ApplicationController
+class UsersController < InheritedResources::Base
+  # actions only: [:create, :new]
+  def create
+    create!
+    session['user_id'] = resource.id
+  end
 end
