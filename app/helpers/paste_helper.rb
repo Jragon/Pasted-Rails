a759097496da173c4b3e2425c851d3791f823b8a
Rory Anderson <rory@jragon.co.uk>
2013-07-14 13:08:13 +0100

    Fixed c++ with gsub

--- app/helpers/paste_helper.rb
+++ app/helpers/paste_helper.rb
@@ -1,9 +1,9 @@
 module PasteHelper
   def highlight code, language
-    raw CodeRay.scan(code, language.name).div(:line_numbers => :table)
+    raw CodeRay.scan(code, language.name.gsub("+", "p").downcase).div(:line_numbers => :table)
   end
 
   def lines code, language
-    CodeRay.scan(code, language.name).loc
+    CodeRay.scan(code, language.name.gsub("+", "p").downcase).loc
   end
 end
